b1f0c8f161d21453941be30238c07eb996df7670
Rose <i499833@fhict.nl>
2024-02-05 21:24:36 +0100

    added login button (no func), added colors to landing screen

--- src/app/page.js
+++ src/app/page.js
@@ -32,27 +32,30 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div>
-
-            <div className="flex overflow-hidden h-screen items-stretch">
+        <div className="h-screen">
+            <div className="flex flex-col items-center ">
+                <button className="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-base font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                    <span className="relative px-6 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Want to edit something?</span>
+                </button>
+            </div>
+            <div className="flex overflow-hidden h-lvh items-stretch">
                 <div
-                    className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105 ">
-                    <div className="m-2">{quote}</div>
+                    className="basis-1/2 grow overflow-hidden m-10 flex-1 h-4/6 items-center justify-center duration-300 hover:scale-105">
+                    <div className="m-2 italic">{quote}</div>
                     <Link href="/quotes">
-                        <div
-                            className="transition ease-out h-5/6 object-cover p-10 rounded-2xl bg-purple-950 hover:bg-indigo-950 ">
-                            <p className="text-white m-2 text-4xl font-bold ">Quotes</p>
-                        </div>
+                        <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                            <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-white dark:bg-gray-950 rounded-md group-hover:bg-opacity-0">Quotes</span>
+                        </button>
                     </Link>
                 </div>
 
-                <div className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105 ">
-                    <div className="m-2">{rizz}</div>
+                <div
+                    className="basis-1/2 grow overflow-hidden m-10 flex-1 h-4/6 items-center justify-center duration-300 hover:scale-105">
+                    <div className="m-2 italic">{rizz}</div>
                     <Link href="/rizz">
-                        <div
-                            className="transition ease-out h-5/6 object-cover p-10 rounded-2xl bg-purple-950 hover:bg-indigo-950">
-                            <p className="text-white m-2 text-4xl font-bold">Rizz</p>
-                        </div>
+                        <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                            <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-white dark:bg-gray-950 rounded-md group-hover:bg-opacity-0">Rizz</span>
+                        </button>
                     </Link>
                 </div>
             </div>
