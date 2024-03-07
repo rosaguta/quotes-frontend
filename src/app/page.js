f5d1eb16358502eff8dcabc1ec89934b3c0b0074
Rose <rvleeuwen@DigitalIndividuals.com>
2024-03-07 21:23:24 +0100

    sparkles on mainpage

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import LoginModal from "@/Components/LoginModal";
 import { toast, ToastContainer } from "react-toastify";
+import { SparklesCore } from "@/Components/ui/sparkles";
 
 const HomePage = () => {
     const [quote, setQuote] = useState("");
@@ -68,40 +69,47 @@ const HomePage = () => {
 
     return (
         <div>
-            <div className="h-screen pt-5 overflow-hidden">
-                <div className="flex flex-col items-center">
-                    <LoginModal isOpen={isModalOpen} toggleModal={toggleModal} onLoginSuccess={handleLoginSuccess} />
-                    <button onClick={toggleModal} className="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-base font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white text-white focus:outline-none focus:ring-cyan-800">
-                        <span className="relative px-6 py-3 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">Want to edit something?</span>
-                    </button>
+            <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={25}
+                className="w-full h-full absolute"
+                particleColor="#FFFFFF"
+            />
+            <div>
+                <div className="h-screen pt-5 overflow-hidden">
+                    <div className="flex flex-col items-center">
+                        <LoginModal isOpen={isModalOpen} toggleModal={toggleModal} onLoginSuccess={handleLoginSuccess} />
+                        <button onClick={toggleModal} className="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-base font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white text-white focus:outline-none focus:ring-cyan-800">
+                            <span className="relative px-6 py-3 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">Want to edit something?</span>
+                        </button>
 
-                </div>
-
-                <div className="flex overflow-hidden h-5/6 items-stretch ">
-                    <div
-                        className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105">
-                        <div className="m-2 italic">{quote}</div>
-                        <Link href="/quotes">
-                            <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:outline-none focus:ring-blue-800">
-                                <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-gray-950 rounded-md group-hover:bg-opacity-0">Quotes</span>
-                            </button>
-                        </Link>
                     </div>
+                    <div className="flex overflow-hidden h-5/6 items-stretch ">
+                        <div
+                            className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105 ">
+                            <div className="m-2 italic">{quote}</div>
+                            <Link href="/quotes">
+                                <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white focus:outline-none focus:ring-blue-800">
+                                    <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-gray-950 rounded-md group-hover:bg-opacity-0">Quotes</span>
+                                </button>
+                            </Link>
+                        </div>
 
-                    <div
-                        className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105">
-                        <div className="m-2 italic">{rizz}</div>
-                        <Link href="/rizz">
-                            <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:outline-none focus:ring-purple-800">
-                                <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-gray-950 rounded-md group-hover:bg-opacity-0">Rizz</span>
-                            </button>
-                        </Link>
+                        <div
+                            className="basis-1/2 grow overflow-hidden m-10 flex-1 items-center justify-center duration-300 hover:scale-105">
+                            <div className="m-2 italic">{rizz}</div>
+                            <Link href="/rizz">
+                                <button className="relative inline-flex items-center justify-center h-5/6 w-full p-1 mb-2 me-2 overflow-hidden font-bold text-6xl rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white text-white focus:outline-none focus:ring-purple-800 ">
+                                    <span className="relative items-center inline-flex justify-center py-3 transition-all ease-in duration-75 h-full w-full bg-gray-950 rounded-md group-hover:bg-opacity-0">Rizz</span>
+                                </button>
+                            </Link>
+                        </div>
                     </div>
-
                 </div>
-
+                <ToastContainer />
             </div>
-            <ToastContainer />
         </div>
     );
 };
