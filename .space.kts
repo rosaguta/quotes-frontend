6196ab9691b1227ebc38cf7c4d40ac02a180af76
Rose <rvleeuwen@DigitalIndividuals.com>
2024-03-14 16:05:26 +0100

    as

--- .space.kts
+++ .space.kts
@@ -1,10 +1,7 @@
 job("Build and push Docker") {
     host("Build artifacts and a Docker image") {
        dockerBuildPush {
-            // Docker context, by default, project root
-            context = "docker"
-            // path to Dockerfile relative to project root
-            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+
             file = "./Dockerfile"
             labels["vendor"] = "DigitalIndividuals"
 
