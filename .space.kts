b91510c8b810d8398427c39f0870160dad1a8369
Rose van Leeuwen <rvleeuwen@DigitalIndividuals.com>
2024-03-15 09:27:39 +0000

    fix?

--- .space.kts
+++ .space.kts
@@ -5,17 +5,13 @@ job("Build and push Docker") {
                 +"main"
             }
         }
-    host("Build artifacts and a Docker image") {
-       dockerBuildPush {
 
-            file = "./Dockerfile"
-            labels["vendor"] = "DigitalIndividuals"
-
-            val spaceRepo = "cfr-r.divsphere.net/q-frontend:latest"
-            // image tags for 'docker push'
-            tags {
-                +"cfr-r.divsphere.net/q-frontend:latest"
-            }
+        file = "./Dockerfile"
+        labels["vendor"] = "DigitalIndividuals"
+        // image tags for 'docker push'
+        tags {
+            +"cfr-r.divsphere.net/q-frontend:latest"
         }
+        
     }
 }
