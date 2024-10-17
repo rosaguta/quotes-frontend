85d7feee1496c0ec85ff79c0f5e4d28e916963e2
Rose <rvleeuwen@DigitalIndividuals.com>
2024-10-17 20:40:35 +0200

    init typescript

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    reactStrictMode: true,
-    swcMinify: true,
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-    images : {
-      domains : ['localhost'] // <== Domain name
-    },
-    webpack: (config, { dev, isServer }) => {
-      // if (!dev && !isServer) {
-      //   Object.assign(config.resolve.alias, {
-      //     "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-      //     react: "preact/compat",
-      //     "react-dom/test-utils": "preact/test-utils",
-      //     "react-dom": "preact/compat",
-      //   });
-      // }
-      return config;
-    }
-  }
+  reactStrictMode: true,
+};
 
 export default nextConfig;
