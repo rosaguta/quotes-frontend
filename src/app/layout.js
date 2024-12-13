497e31c354f45c38dbc35e9c942cc058836ba30a
Rose van Leeuwen | Digital Individuals <rvleeuwen@digitalindividuals.com>
2024-12-13 09:54:27 +0100

    docker doesnt use system env so this must do

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <UserProvider>
         <body className={inter.className}>{children}</body>    
-      </UserProvider>
     </html>
   );
 }
