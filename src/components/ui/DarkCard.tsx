2c9dc7a7148998f300c3c10535f01444a27bffae
Rose van Leeuwen <guta.rob@hotmail.nl>
2025-01-19 12:29:05 +0100

    phone view stuff

--- src/components/ui/DarkCard.tsx
+++ src/components/ui/DarkCard.tsx
@@ -1,33 +1,66 @@
+"use client"
 import React from 'react';
 import { motion } from 'framer-motion';
 import DotPattern from './DotPattern';
 import { cn } from '@/lib/utils';
-const DarkCard = ({ children, color, borderColor=null }) => {
+const DarkCard = ({ children, color, borderColor = null }) => {
   const [isHovered, setIsHovered] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false)
 
+  React.useEffect(() => {
+    setIsMobile(window.innerWidth <= 768)
+
+    // const handleResize = () => {
+    //   setIsMobile(window.innerWidth <= 768)
+    //   setIsHovered(true)
+    // }
+
+    // window.addEventListener('resize', handleResize)
+    // return () => window.removeEventListener('resize', handleResize)
+  }, [])
+  console.log(isHovered)
   return (
     <motion.div
       className="relative max-w-md p-6 bg-black rounded-xl border-[0.5px] overflow-hidden"
       onHoverStart={() => setIsHovered(true)}
       onHoverEnd={() => setIsHovered(false)}
-      whileHover={{ borderColor: borderColor  }}
+      whileHover={{ borderColor: borderColor }}
     >
       <div className="relative z-10">
         {children}
       </div>
+      {/* desktop */}
+      <div className="hidden md:block"> 
+        <DotPattern
+          width={12}
+          height={12}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)]")}
+          style={{
+            fill: isHovered ? color : "rgba(156, 163, 175, 0.8)", // Adjusted to hover state
+            transition: "fill 0.2s ease-in-out"
+          }}
+        />
+      </div>
+      {/* phone */}
+      <div className='block md:hidden'> 
       <DotPattern
-        width={12}
-        height={12}
-        cx={1}
-        cy={1}
-        cr={1}
-        className={cn(
-          "[mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)]")}
-        style={{
-          fill: isHovered ? color : "rgba(156, 163, 175, 0.8)", // Adjusted to hover state
-          transition: "fill 0.2s ease-in-out"
-        }}
-      />
+          width={12}
+          height={12}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)]")}
+          style={{
+            fill: true ? color : "rgba(156, 163, 175, 0.8)", // Adjusted to hover state
+            transition: "fill 0.2s ease-in-out"
+          }}
+        />
+      </div>
     </motion.div>
   );
 };
