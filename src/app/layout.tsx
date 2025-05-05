2927fe5b4ea9b8d3410de620c6a9ec039d9d23a6
Rose van Leeuwen <guta.rob@hotmail.nl>
2025-05-05 23:07:33 +0200

    new pic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,19 +14,17 @@ export default function RootLayout({ children }: any) {
       <head>
         <title>Quotes</title>
         <meta name="description" content="Find all the crazy and wacky quotes on this website :3" />
-
         <meta property="og:title" content="Quotes" />
         <meta property="og:description" content="Find all the crazy and wacky quotes on this website :3" />
-        <meta property="og:image" content="https://quotes.divsphere.net/quote.svg" />
+        <meta property="og:image" content="https://quotes.divsphere.net/femboynt.png" />
         <meta property="og:url" content="https://quotes.divsphere.net/" />
         <meta property="og:type" content="website" />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Quotes" />
         <meta name="twitter:description" content="Find all the crazy and wacky quotes on this website :3" />
-        <meta name="twitter:image" content="https://quotes.divsphere.net/quote.svg" />
+        <meta name="twitter:image" content="https://quotes.divsphere.net/femboynt.png" />
       </head>
-      <body>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
